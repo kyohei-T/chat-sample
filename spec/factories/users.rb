6ce50bca47fddb1b407185780ca27edb8a7d771b
kyohei-T <tomino@Tommy-MacBook-Pro.local>
2019-03-21 18:56:26 +0900

    prepare test
    rspec
    factorybot
    faker
    rails controller testing

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,1 +1,9 @@
 FactoryBot.define do
+  factory :user do
+    password = Faker::Internet.password(8)
+    name {Faker::Name.last_name}
+    eamil {Faker::Internet.free_email}
+    password {password}
+    password_confirmation {password}
+  end
+end
